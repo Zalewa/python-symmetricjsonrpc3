b275cf93c4ffdaf40cde77ff747dd0ced0e8dba2
Robikz <zalewapl@gmail.com>
2024-06-03 10:33:19 +0200

    feat(examples): add new parameters and logging
    Add `--host`, `--port`, `--verbose` and `--quiet` parameters to both
    client.py and server.py examples. Handle them with Python's 'argparse'
    module.
    
    Introduce an in-place logging system, so that the 'verbose' and 'quiet'
    params actually do something. Log on the COMM, INFO and DEBUG levels to
    inform what the scripts are doing.

--- examples/client.py
+++ examples/client.py
@@ -21,28 +21,55 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA
-import sys
+import argparse
 import symmetricjsonrpc3
+
+
+g_loglevel = 0
+
+COMM = -1
+INFO = 0
+DEBUG = 1
+
+
+def log(level, *args, **kwargs):
+    if g_loglevel >= level:
+        print(*args, **kwargs)
 
 
 class PingRPCClient(symmetricjsonrpc3.RPCClient):
     class Request(symmetricjsonrpc3.RPCClient.Request):
         def dispatch_request(self, subject):
             # Handle callbacks from the server
-            print("dispatch_request(%s)" % (repr(subject),))
+            log(COMM, f"-> REQ: dispatch_request({repr(subject)})")
             assert subject['method'] == "pingping"
             return "pingpong"
 
 
-if '--help' in sys.argv:
-    print("""client.py
-    --ssl
-        Encrypt communication with SSL using M2Crypto. Requires a
-        server.pem in the current directory.
-""")
-    sys.exit(0)
+def parse_args():
+    global g_loglevel
 
-if '--ssl' in sys.argv:
+    argp = argparse.ArgumentParser()
+    argp.add_argument("-H", "--host", default="localhost",
+                      help="host to connect to [%(default)s]")
+    argp.add_argument("-p", "--port", default=4712,
+                      help="port to connect to [%(default)s]")
+    argp.add_argument("-q", "--quiet", default=0, action="count",
+                      help="decrease verbosity level")
+    argp.add_argument("-v", "--verbose", default=0, action="count",
+                      help="increase verbosity level")
+    argp.add_argument("--ssl", action="store_true", help=(
+        "Encrypt communication with SSL using M2Crypto. "
+        "Requires a server.pem in the current directory."))
+
+    args = argp.parse_args()
+    g_loglevel = args.verbose - args.quiet
+    return args
+
+
+args = parse_args()
+
+if args.ssl:
     # Set up an SSL connection
     import M2Crypto
     ctx = M2Crypto.SSL.Context()
@@ -56,17 +83,25 @@ else:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #  Connect to the server
-s.connect(('localhost', 4712))
+log(INFO, f"Connecting to ({args.host}:{args.port}) ...")
+s.connect((args.host, args.port))
+log(DEBUG, f"Connected to ({args.host}:{args.port})")
 
 # Create a client thread handling for incoming requests
+log(DEBUG, "Creating Pong client ...")
 client = PingRPCClient(s)
 
 # Call a method on the server
+log(INFO, "Sending 'ping' request ...")
 res = client.request("ping", wait_for_response=True)
-print("client.ping => %s" % (repr(res),))
+log(COMM, f"-> RES: client.ping => {repr(res)}")
 assert res == "pong"
 
 # Notify server it can shut down
+log(DEBUG, "Telling server to shut down ...")
 client.notify("shutdown")
 
+log(DEBUG, "Shutting down ourselves ...")
 client.shutdown()
+
+log(INFO, "Done!")
--- examples/server.py
+++ examples/server.py
@@ -21,8 +21,20 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA
-import sys
+import argparse
 import symmetricjsonrpc3
+
+
+g_loglevel = 0
+
+COMM = -1
+INFO = 0
+DEBUG = 1
+
+
+def log(level, *args, **kwargs):
+    if g_loglevel >= level:
+        print(*args, **kwargs)
 
 
 class PongRPCServer(symmetricjsonrpc3.RPCServer):
@@ -30,7 +42,7 @@ class PongRPCServer(symmetricjsonrpc3.RPCServer):
         class Thread(symmetricjsonrpc3.RPCServer.InboundConnection.Thread):
             class Request(symmetricjsonrpc3.RPCServer.InboundConnection.Thread.Request):
                 def dispatch_notification(self, subject):
-                    print("dispatch_notification(%s)" % (repr(subject),))
+                    log(COMM, "-> NOT: dispatch_notification(%s)" % (repr(subject),))
                     assert subject['method'] == "shutdown"
                     # Shutdown the server. Note: We must use a
                     # notification, not a method for this - when the
@@ -39,26 +51,39 @@ class PongRPCServer(symmetricjsonrpc3.RPCServer):
                     self.parent.parent.parent.shutdown()
 
                 def dispatch_request(self, subject):
-                    print("dispatch_request(%s)" % (repr(subject),))
+                    log(COMM, "-> REQ: dispatch_request(%s)" % (repr(subject),))
                     assert subject['method'] == "ping"
                     # Call the client back
                     # self.parent is a symmetricjsonrpc3.RPCClient subclass
-                    # (see the client code for more examples)
                     res = self.parent.request("pingping", wait_for_response=True)
-                    print("parent.pingping => %s" % (repr(res),))
+                    log(COMM, "-> RES: parent.pingping => %s" % (repr(res),))
                     assert res == "pingpong"
                     return "pong"
 
 
-if '--help' in sys.argv:
-    print("""client.py
-    --ssl
-        Encrypt communication with SSL using M2Crypto. Requires a
-        server.pem and server.key in the current directory.
-""")
-    sys.exit(0)
+def parse_args():
+    global g_loglevel
 
-if '--ssl' in sys.argv:
+    argp = argparse.ArgumentParser()
+    argp.add_argument("-H", "--host", default="localhost",
+                      help="hostname to listen on [%(default)s]")
+    argp.add_argument("-p", "--port", default=4712,
+                      help="port to listen on [%(default)s]")
+    argp.add_argument("-q", "--quiet", default=0, action="count",
+                      help="decrease verbosity level")
+    argp.add_argument("-v", "--verbose", default=0, action="count",
+                      help="increase verbosity level")
+    argp.add_argument("--ssl", action="store_true", help=(
+        "Encrypt communication with SSL using M2Crypto. "
+        "Requires a server.pem and server.key in the current directory."))
+
+    args = argp.parse_args()
+    g_loglevel = args.verbose - args.quiet
+    return args
+
+
+args = parse_args()
+if args.ssl:
     # Set up a SSL socket
     import M2Crypto
     ctx = M2Crypto.SSL.Context()
@@ -71,11 +96,22 @@ else:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 #  Start listening on the socket for connections
-s.bind(('', 4712))
+log(DEBUG, f"Binding server socket to ({args.host}:{args.port}) ...")
+s.bind((args.host, args.port))
 s.listen(1)
+log(INFO, f"Listening on ({args.host}:{args.port}) ...")
 
 # Create a server thread handling incoming connections
+log(DEBUG, "Creating Pong server ...")
 server = PongRPCServer(s, name="PongServer")
 
-# Wait for the server to stop serving clients
-server.join()
+try:
+    log(INFO, "Serving clients ...")
+    # Wait for the server to stop serving clients
+    server.join()
+except KeyboardInterrupt:
+    log(INFO, "Shutting down the server ...")
+    server.shutdown()
+    log(DEBUG, "Awaiting server shutdown ...")
+    server.join()
+log(INFO, "Done!")
