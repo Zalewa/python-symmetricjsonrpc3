5faf0ea5cb2f851eefc88c3c4378cbdaafb0bddd
Robikz <zalewapl@gmail.com>
2024-06-14 11:57:23 +0200

    feat(io): add fd mode detectors and the makefile() wrapper
    The makefile() function can convert a variety of file-descriptors to
    Python's file-like objects that share the common read()/write()/close()
    interface.

--- src/symmetricjsonrpc3/io.py
+++ src/symmetricjsonrpc3/io.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 # python-symmetricjsonrpc3
 # Copyright (C) 2024 Robert "Robikz" Zalewski <zalewapl@gmail.com>
 #
@@ -16,15 +17,244 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA
+"""IO wrappers for file-descriptor malleability."""
+import errno
+import fcntl
 import io
 import os
 import queue
 import selectors
+import socket
+import sys
 import threading
 from logging import getLogger
 
 
 logger = getLogger(__name__)
+
+
+class Mode:
+    """Get info from the "mode" text (as in `open(mode=...)`)."""
+    def __init__(self, mode):
+        self.mode = mode
+
+    @property
+    def read(self):
+        """True if mode allows reading."""
+        return any(flag in self.mode in self.mode for flag in "r+")
+
+    @property
+    def write(self):
+        """True if mode allows writing."""
+        return any(flag in self.mode for flag in "aw+")
+
+    @property
+    def typemode(self):
+        """Get 't' for text or 'b' for binary."""
+        return 'b' if 'b' in self.mode else 't'
+
+    @property
+    def binary(self):
+        """True if opened in binary mode."""
+        return self.typemode == 'b'
+
+    @property
+    def text(self):
+        """True if opened in text mode."""
+        return self.typemode == 't'
+
+
+class _UnknownMode(Mode):
+    read = True
+    write = True
+    typemode = ''
+
+    def __init__(self):
+        super().__init__("")
+
+
+_m_unknown_mode = _UnknownMode()
+
+
+def rwmode(fd):
+    """Get read/write mode of a file-descriptor (or a file-like).
+
+    The `fd` can be anything file-like that `makefile()` would also
+    accept.
+
+    Return "r", "w" or "r+". Return empty str if unable to
+    determine. Return "w" also on "a" mode. Return "r+" on
+    all "*+" modes.
+
+    Raise OSError with errno.EBADF if it's not a file-descriptor
+    nor a file-like.
+    """
+    def _fd_mode_methods_to_flags(fd, mode):
+        """Heuristically discover file-like objects.
+
+        Yeah, this is a guess-work, and an assumption that the methods
+        that are named 'read' and 'write' actually behave as expected.
+        """
+        flags = ""
+        if hasattr(fd, "read") and mode.read:
+            flags += "r"
+        if hasattr(fd, "write") and mode.write:
+            flags += "w"
+        if flags == "rw":
+            flags = "r+"
+        return flags
+
+    # Step 1:
+    # When we're dealing with a Python IOBase object, its mode
+    # must be taken as the source of the absolute truth, regardless
+    # the underlying's fileno flags.
+    if isinstance(fd, io.IOBase) and hasattr(fd, "mode"):
+        mode = Mode(fd.mode)
+        return _fd_mode_methods_to_flags(fd, mode)
+
+    # Step 2:
+    # Try to get the info from the system first, if possible.
+    try:
+        flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+        if (flags & os.O_WRONLY) == os.O_WRONLY:
+            return "w"
+        elif (flags & os.O_RDWR) == os.O_RDWR:
+            return "r+"
+        else:
+            return "r"
+    except (TypeError, io.UnsupportedOperation):
+        pass
+
+    # Step 3:
+    # If the system fails, look around the object.
+    if not hasattr(fd, "read") and not hasattr(fd, "write"):
+        raise OSError(errno.EBADF, os.strerror(errno.EBADF), fd)
+
+    flags = ""
+    if hasattr(fd, "mode") and isinstance(fd.mode, str):
+        mode = Mode(fd.mode)
+    else:
+        mode = _m_unknown_mode
+    return _fd_mode_methods_to_flags(fd, mode)
+
+
+def typemode(fd):
+    """Get the text/binary mode of a file-descriptor (or a file-like).
+
+    The `fd` can be anything filelike that `makefile()` would also
+    accept.
+
+    Return 't' for text, 'b' for binary or an empty str if unknown.
+
+    Raise OSError with errno.EBADF if it's not a file-descriptor
+    nor a file-like.
+    """
+    if isinstance(fd, int):
+        fcntl.fcntl(fd, fcntl.F_GETFD)
+        return "b"
+    elif isinstance(fd, (io.RawIOBase, io.BufferedIOBase, socket.socket)):
+        return "b"
+    elif isinstance(fd, io.TextIOBase):
+        return "t"
+    elif hasattr(fd, "mode") and isinstance(fd.mode, str):
+        return Mode(fd.mode).typemode
+    return ""
+
+
+class BytesIOWrapper(io.RawIOBase):
+    def __init__(self, file, encoding=None, errors='strict'):
+        self.file = file
+        self.encoding = encoding or sys.getdefaultencoding()
+        self.errors = errors
+        self.buf = b''
+
+    def readinto(self, buf):
+        if not self.buf:
+            self.buf = self.file.read(4096).encode(self.encoding, self.errors)
+            if not self.buf:
+                return 0
+        length = min(len(buf), len(self.buf))
+        buf[:length] = self.buf[:length]
+        self.buf = self.buf[length:]
+        return length
+
+    def write(self, buf):
+        text = buf.decode(self.encoding, self.errors)
+        nwritten = self.file.write(text)
+        return len(buf) if nwritten == len(text) else len(text[:nwritten].encode())
+
+
+def makefile(fd, mode=None, **kwargs):
+    """Wrap anything file-like in a file-like object.
+
+    The `fd` is assumed to be an open file (i.e. not a path).
+
+    If `fd`:
+
+    - is an int -- consider it a file-descriptor and os.fdopen() it,
+    - looks like a socket -- socket.makefile() it, with a monkey-patched
+      close() that will actually call socket.close(),
+    - looks like a file-like object already,
+      - and matches the text/binary mode -- just return it,
+      - and has a different text/binary mode -- wrap it in a codec,
+    - none of the above -- raise TypeError.
+
+    The `mode` is as in `open()`, but it will be tested against the `fd`
+    to check if the read-write mode matches, and if conversion between
+    binary or text is needed. Truncation, repositioning and appending
+    modes may be ignored by the wrapper. If `None`, `makefile` will try
+    to match the `fd` mode.
+
+    If the requested read-write `mode` doesn't match the mode of `fd`,
+    ValueError is raised.
+
+    If the text/binary `mode` differs, `fd` will be put into a
+    conversion wrapper. The `encoding`, `errors`, et al parameters can
+    be embedded in **kwargs.
+
+    """
+    wrapper = None
+    if isinstance(fd, int):
+        if mode is None:
+            fd_rwmode = rwmode(fd)
+            mode = fd_rwmode + "b"
+        return os.fdopen(fd, mode=mode, **kwargs)
+    elif isinstance(fd, io.IOBase):
+        fd_rwmode = rwmode(fd)
+        fd_typemode = typemode(fd)
+        fd_mode = Mode(fd_rwmode + fd_typemode)
+        req_mode = Mode(mode) if mode is not None else fd_mode
+        if ((req_mode.write and not fd_mode.write)
+                or (req_mode.read and not fd_mode.read)):
+            raise ValueError(f"read-write mode mismatch mode={mode},fd.mode={fd_mode.mode}")
+        if req_mode.typemode == fd_mode.typemode:
+            return fd
+        else:
+            # Wrap into a converter.
+            if req_mode.text:
+                # binary fd to text wrapper
+                wrapper = io.TextIOWrapper(fd, **kwargs)
+            else:
+                # text fd to binary wrapper
+                wrapper = BytesIOWrapper(fd, **kwargs)
+    elif isinstance(fd, socket.socket):
+        req_mode = Mode(mode or "rwb")
+        sockmode = ("r" if req_mode.read else ""
+                    + "w" if req_mode.write else ""
+                    + "b" if req_mode.binary else "")
+        wrapper = fd.makefile(mode=sockmode, **kwargs)
+    else:
+        raise TypeError(f"don't know how to make a file out of {type(fd)}")
+
+    # Monkey-patch the wrapper's close function so that
+    # closing the wrapper also closes the underlying file.
+    original_close = wrapper.close
+
+    def monkey_close(*args, **kwargs):
+        original_close(*args, **kwargs)
+        fd.close()
+    wrapper.close = monkey_close
+    return wrapper
 
 
 class _Reselector:
